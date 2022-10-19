40dc06d527798baa0de7bb62f938ecf0026ea29e
leo78450 <harpiemangue37@gmail.com>
2022-10-19 17:11:34 +0200

    v1 monitor_processus

--- first_project/monitor_processus.go
+++ first_project/monitor_processus.go
@@ -2,19 +2,50 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
-	"path/filepath"
+	"unicode"
 )
 
-func main() {
-	matches, err := filepath.Glob("/proc/*/exe")
+type Procs struct {
+	PID string
+	Pwd string
+	Cwd string
+}
+
+func all_procs(procs []Procs) []Procs {
+	var proc Procs
+	files, err := ioutil.ReadDir("/proc/")
 	if err != nil {
-		os.Exit(2)
+		log.Fatal(err)
 	}
-	for _, file := range matches {
-		target, _ := os.Readlink(file)
-		if len(target) > 0 {
-			fmt.Printf("%+v\n", target)
+	for _, file := range files {
+		if file.IsDir() && unicode.IsDigit(rune(file.Name()[0])) {
+			proc.PID = file.Name()
+			target, _ := os.Readlink("/proc/" + file.Name() + "/exe")
+			if len(target) > 0 {
+				proc.Pwd = target
+			}
+			target2, _ := os.Readlink("/proc/" + file.Name() + "/cwd")
+			if len(target2) > 0 {
+				proc.Cwd = target2
+			}
+			procs = append(procs, proc)
 		}
 	}
+	return procs
+}
+
+func print_all() {
+	var procs []Procs
+	fmt.Println("PID\tEXE\tCWD\t% of use")
+	procs = all_procs(procs)
+	for _, proc := range procs {
+		fmt.Println(proc.PID, proc.Pwd, proc.Cwd)
+	}
+}
+
+func main() {
+	print_all()
 }
