f9a88a5561b7eb677c4b61b4d7bfc7b102483f6c
leo78450 <harpiemangue37@gmail.com>
2022-10-20 09:56:40 +0200

    add comment

--- first_project/monitor_processus.go
+++ first_project/monitor_processus.go
@@ -21,27 +21,32 @@ type Procs struct {
 
 func all_procs(procs []Procs) []Procs {
 	var proc Procs
+	//reads all files located in /proc
 	files, err := ioutil.ReadDir("/proc/")
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, file := range files {
+		//all files whose name begins with a number
 		if file.IsDir() && unicode.IsDigit(rune(file.Name()[0])) {
 			proc.PID = file.Name()
+			//read the file exe relative to the file contained in the variable file.Name()
 			target, _ := os.Readlink("/proc/" + file.Name() + "/exe")
 			if len(target) > 0 {
 				proc.Pwd = target
 			} else {
 				proc.Pwd = "Need privileges"
 			}
+			//read the file cwd relative to the file contained in the variable file.Name()
 			target2, _ := os.Readlink("/proc/" + file.Name() + "/cwd")
 			if len(target2) > 0 {
 				proc.Cwd = target2
 			} else {
 				proc.Cwd = "Need privileges"
 			}
+			//read the file smaps relative to the file name contained in the variable file.Name()
 			f, err := os.Open("/proc/" + file.Name() + "/smaps")
-			//condition error
+			//condition error if we don't have the permission to read the file
 			if err != nil {
 				proc.Size = "Size: \t\tNeed privileges"
 				proc.Rss = "Rss: \t\tNeed privileges"
@@ -66,6 +71,7 @@ func all_procs(procs []Procs) []Procs {
 				}
 				line++
 			}
+			//append to procs any new data added to proc
 			procs = append(procs, proc)
 		}
 	}
@@ -73,8 +79,11 @@ func all_procs(procs []Procs) []Procs {
 }
 
 func print_all() {
+	//procs is a structure table
 	var procs []Procs
+	//append to procs all new data
 	procs = all_procs(procs)
+	//print all data
 	for _, proc := range procs {
 		fmt.Println(" PID:\t\t", proc.PID, "\n PWD:\t\t", proc.Pwd, "\n CWD:\t\t", proc.Cwd, "\n", proc.Size, "\n", proc.Rss, "\n", proc.Pss, "\n", proc.Swap, "\n------")
 	}
